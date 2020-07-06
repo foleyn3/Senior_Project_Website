34737039d9abebae394c8ae0455e6c3c8fbda6c0
nfoley <nfoley58@gmail.com>
2020-07-06 11:12:12 -0400

    Registration for website stuff

---
+++ registertest/connectserverdb.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'seniorproj-db.cqiexl3pxle4.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'password22');
+define('DB_NAME', 'registration');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+echo "Connected";
+?>
